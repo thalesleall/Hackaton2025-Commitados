09e03c684c06f1b6e99b7e8a79d68b33015526d0
Diego de Oliveira Murari Guimarães <diego_murari@hotmail.com>
2025-09-27 17:07:36 -0300

    feat: primeira versão com Central e ChatBot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
-import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Central from "./pages/Central";
-import Paciente from "./pages/Paciente";
-import Agendamentos from "./pages/Agendamentos";
-import Perfil from "./pages/Perfil";
-import Ajuda from "./pages/Ajuda";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom"
+
+// pages
+import Login from "./pages/Login"
+import Central from "./pages/Central"
+import Paciente from "./pages/Paciente"
+import Agendamentos from "./pages/Agendamentos"
+import Perfil from "./pages/Perfil"
+import Ajuda from "./pages/Ajuda"
 
 function App() {
-
   return (
-        <Router>
-          <Routes>
-            <Route path="/" element={<Navigate to={'/login'}/>}/>
-            <Route path='/login' element={<Login />} />
-            <Route path='/login' element={<Login />} />
-            <Route path="/central" element={<Central />} />
-            <Route path="/beneficiario" element={<Paciente />} />
-            <Route path="/agendamentos" element={<Agendamentos />} />
-            <Route path="/perfil" element={<Perfil />} />
-            <Route path="/ajuda" element={<Ajuda />} />
-          </Routes>
-        </Router>
+    <Router>
+      <Routes>
+        {/* redireciona / para /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* rotas principais */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/central" element={<Central />} />
+        <Route path="/beneficiario" element={<Paciente />} />
+        <Route path="/agendamentos" element={<Agendamentos />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/ajuda" element={<Ajuda />} />
+
+        {/* rota coringa (404) */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
   )
 }
 
